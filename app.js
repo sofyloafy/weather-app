ef5b46d408c11c293268ac1fa01867ea4755a9ac
Sophie <sophiebrown438@gmail.com>
2020-08-26 13:16:00 +0100

    API data to display'

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 window.addEventListener('load', () => {
   let long
   let lat
+  let temperatureDescription = document.querySelector('.temperature-description')
+  let temperatureDegree = document.querySelector('.temperature-degree')
+  let locationTimezone = document.querySelector('.location-timezone')
+
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude
       lat = position.coords.latitude
+      // const proxy = 'https://cors-anywhere.herokuapp.com/'
       const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3de56cafefd2787dd31581eaa1f6de25`
       fetch(api)
       .then(response => {
         return response.json()
       })
       .then(data => {
-        console.log(data)
+        const {temp} = data.main
+        const feels_like = data.weather[0].description
+        locationTimezone.textContent = data.name
+        let tempinC = Math.floor(temp - 273.15)
+        temperatureDegree.textContent = tempinC
+        temperatureDescription.textContent = feels_like
+
       })
     })
 
