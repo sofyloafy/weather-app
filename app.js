b6f79b6a045993ddc4cd3b42c15f763f7bbaf07d
Sophie <sophiebrown438@gmail.com>
2020-08-26 12:29:10 +0100

    Connection established weather API

--- app.js
+++ app.js
@@ -5,10 +5,21 @@ window.addEventListener('load', () => {
     navigator.geolocation.getCurrentPosition(position => {
       long = position.coords.longitude
       lat = position.coords.latitude
-
+      const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=3de56cafefd2787dd31581eaa1f6de25`
+      fetch(api)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        console.log(data)
+      })
     })
+
+  
   }
   // else{
   //   h1.textContent = "Please let Google know your location!"
   // }
 })
+
+// working api : http://api.openweathermap.org/data/2.5/weather?lat=50&lon=-0.6793005&appid=3de56cafefd2787dd31581eaa1f6de25
